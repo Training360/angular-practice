827a761430ea9982b79adcca638dbb2eba6b921c
cherryApp <j.cserko@gmail.com>
2019-08-13 11:19:42 +0200

    Feat: fs practice has been done.

--- nodejs-getting-started/server.js
+++ nodejs-getting-started/server.js
@@ -20,8 +20,25 @@ class PostHandler {
 
         // Megjött az összes adat.
         req.on('end', () => {
+            this.allData = JSON.parse(this.allData);
+
+            fs.readFile('./json/users.json', 'utf8', (err, jsonString) => {
+                if (err) {
+                    return res.end(JSON.stringify(err));
+                }
+                let users = JSON.parse(jsonString);
+                users.push( this.allData );
+
+                fs.writeFile('./json/users.json', JSON.stringify(users, null, 4), 'utf8', (err) => {
+                    if (err) {
+                        return res.end(JSON.stringify(err));
+                    }
+                    res.end('Köszi.');
+                });
+
+            });
+
             console.log( this.allData );
-            res.end('Köszi.');
         });
     }
 }
