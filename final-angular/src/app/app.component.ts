ea0afb730fb39774adf31608286487d9a9b9374d
cherryApp <j.cserko@gmail.com>
2019-09-13 12:45:01 +0200

    Feat: init base Angular

---
+++ final-angular/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { UserService } from './service/user.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  list$: Observable<any> = this.us.read();
+
+  constructor(private us:UserService) {
+    us.access();
+  }
+  title = 'final-angular';
+}
---
+++ final-angular/src/app/service/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url: string = 'http://localhost:3000/api/users';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  access(): void {
+    this.http.get(this.url).subscribe(
+      (data) => console.log(data)
+    )
+  }
+
+  read():Observable<any>{
+    return this.http.get(this.url);
+
+  }
+}
