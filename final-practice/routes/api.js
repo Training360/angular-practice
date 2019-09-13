50c9069495d7c21c037347432360d040b94644e3
cherryApp <j.cserko@gmail.com>
2019-09-13 12:41:22 +0200

    Feat: base angular

--- final-practice/routes/api.js
+++ final-practice/routes/api.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const DB = require("./../modules/db");
+const db = new DB();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'Server works!' });
+router.get('/', (req, res, next) => {
+    res.json({message: 'Server works'});
+})
+
+router.get('/users', async (req, res, next) => {
+    let result = await db.read();
+    res.json(result);
 });
 
 module.exports = router;
